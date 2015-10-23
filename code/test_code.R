4a6ef4b23a9d2e570a91498277375aac54bae567
moosik <vitalina@git>
2015-10-23 10:17:36 -0700

    Fixed translation error in the database; wrote a function that selects a random number of sentences to be tested

--- code/test_code.R
+++ code/test_code.R
@@ -7,7 +7,7 @@ spanish <- sapply(temp, "[", 2)
 lv <- sapply(temp, "[", 3)
 data.df <- data.frame(english, spanish, level = lv, stringsAsFactors = FALSE)
 
-# Version 1: select n random sentences, go throug them,
+# Version 1: select n random sentences, go through them,
 # present with the english version, read my translation, present both results,
 # ask whether I was correct or not, count the number of correct answers, present
 # with the results
@@ -15,20 +15,38 @@ data.df <- data.frame(english, spanish, level = lv, stringsAsFactors = FALSE)
 transTest <- function(data, howmany){
   # Indices for the rows to select
   select.rows <- sample(1:nrow(data), howmany)
-  tobe.tested <- data[select.rows]
-  cat(paset("Will test", howmany, "sentences", sep = " "))
+  # Select the data
+  tobe.tested <- data[select.rows, ]
+  # List to store the sentences which weren't translated correctly
+  tobe.studied <- list()
+  cat(paste("Will test", howmany, "sentences", sep = " "))
+  cat("\n")
   score <- 0
-  cat("Initial score is", score, sep = " ")
-  cat("Starting the sentences...")
+  cat(paste("Initial score is", score, sep = " "))
+  cat("\n")
+  cat("Starting the sentences...\n")
   for(i in seq_len(nrow(tobe.tested))){
     # wait for the input
-    answer = readline(prompt =  paste(tobe.tested[i,1]," ", sep = ""))
+    answer <- readline(prompt =  paste("Sentence: ", tobe.tested[i,1], ". Your translation: ", sep = ""))
     cat(paste("You entered:", answer, sep = " "))
+    cat("\n")
     cat(paste("The correct translation:", tobe.tested[i,2], sep = " "))
     count.ch <- readline(prompt = "Were you correct? Enter y(yes) or n(no) ")
-    res <- trimws(as.character(count.ch), what = "both")
+    res <- trimws(as.character(count.ch), which = "both")
     if(res == "y"){
       score <- score +1
     }
+    else{
+      tobe.studied[[length(tobe.studied) + 1]] <- tobe.tested[i,1]
+    }
+  }
+  cat(paste("Your score is ", round((score/nrow(tobe.tested))*100, 2), "%", sep = ""))
+  cat("\n")
+  cat("Sentences you didn't translate correctly:\n")
+  j <- 1
+  while(j <= length(tobe.studied)){
+    cat(tobe.studied[[j]])
+    cat("\n")
+    j <- j + 1
   }
 }
