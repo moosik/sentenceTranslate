6b42bfc0fefc754249e7649098dbca090f7ef934
moosik <vitalina@git>
2015-10-31 19:01:45 -0700

    changed the code for the default shiny example to show the number of sentences per level as a histogram. Learned that I can have R code in ui.R to provide input to the slider

--- plotSentencesByLevelApp/server.R
+++ plotSentencesByLevelApp/server.R
@@ -1,7 +1,16 @@
 library(shiny)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  # I think data processing doesn't need to be reactive:
+  data<- readLines("../data/SpanishSentences.txt")
+  temp <- strsplit(data, split = ";")
+  english <- sapply(temp, "[", 1)
+  spanish <- sapply(temp, "[", 2)
+  lv <- as.numeric(sapply(temp, "[", 3))
+  data.df <- data.frame(english, spanish, level = lv, stringsAsFactors = FALSE)
+  data.df <- na.omit(data.df)
   
   # Expression that generates a histogram. The expression is
   # wrapped in a call to renderPlot to indicate that:
@@ -11,10 +20,15 @@ shinyServer(function(input, output) {
   #  2) Its output type is a plot
   
   output$distPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # Get the levels
+    # x    <- data.df$level  
+    # bins <- seq(min(data.df$level), max(data.df$level), 
+    #            length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    ggplot(data.df, aes(level)) + 
+      geom_histogram(binwidth = input$bins, color = "white", fill = "grey") +
+      ggtitle("Sentences per level") + theme_bw()
   })
 })
--- plotSentencesByLevelApp/ui.R
+++ plotSentencesByLevelApp/ui.R
@@ -1,19 +1,25 @@
 library(shiny)
-
+data<- readLines("../data/SpanishSentences.txt")
+temp <- strsplit(data, split = ";")
+english <- sapply(temp, "[", 1)
+spanish <- sapply(temp, "[", 2)
+lv <- as.numeric(sapply(temp, "[", 3))
+data.df <- data.frame(english, spanish, level = lv, stringsAsFactors = FALSE)
+data.df <- na.omit(data.df)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Hello Shiny!"),
+  titlePanel("How many sentences we have for each level"),
   
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+                  "Bin width to use:",
+                  min = min(data.df$level) + 1,
+                  max = max(data.df$level),
+                  value = 5)
     ),
     
     # Show a plot of the generated distribution
