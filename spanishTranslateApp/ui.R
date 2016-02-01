8b777a903417c9604958570dbb0d1dd42428f92a
moosik <vitalina@git>
2016-02-01 09:36:16 -0800

    created ugly and rough layout

--- spanishTranslateApp/ui.R
+++ spanishTranslateApp/ui.R
@@ -1,55 +1,52 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Test your Spanish Vocabulary from Duolingo"),
-  sidebarLayout(
-    
-    sidebarPanel(
-      
-      # Panel for the choices of sections from the database to be testes
-      uiOutput("sectionslider"),
-      
-      # Panel for the number of sentences to be tested
-      sliderInput("numbersentences", "Choose number of sentences", 
-                  min = 5, max = 30, value = 15, step = 1),
-      
-      # Button to submit the sections and the number of sentences
-      actionButton("submitparameters", "Submit")
-      
+  #titlePanel("Test your Spanish Vocabulary from Duolingo"),
+  fluidRow(
+    column(11, "boohoo"),
+    fluidRow(
+      column(3,
+             # Panel for the choices of sections from the database to be testes
+             uiOutput("sectionslider"),
+             
+             # Panel for the number of sentences to be tested
+             sliderInput("numbersentences", "Choose number of sentences", 
+                         min = 5, max = 30, value = 15, step = 1),
+             
+             # Button to submit the sections and the number of sentences
+             actionButton("submitparameters", "Submit")),
+        column(8,
+               textOutput("sectionsToTest"),
+               textOutput("sentencesAvailable"),
+               textOutput("sentencesWillTest"))
       ),
-    
-    mainPanel(
-      
-      # Output what sections, how many sentences available and how many
-      # sentences will be tested after the "submitparameters" was pressed
-      textOutput("sectionsToTest"),
-      textOutput("sentencesAvailable"),
-      textOutput("sentencesWillTest"),
-      # Add section the presents Spanish question: button Spanish and the
-      # output with the Spanish sentence
-      conditionalPanel("input.submitparameters",
-                       actionButton("spanish", "Spanish")),
-      
-      conditionalPanel("input.spanish",
-                       textOutput("spanishS")),
-      # Add conditional panel for the English translation
-      conditionalPanel("input.submitparameters",
-                       actionButton("english", "English")),
-      
-      conditionalPanel("input.english",
-                       textOutput("englishS")),
-      # create a conditional panel based on english button
-      conditionalPanel("input.english",
-                       helpText("Was your translation correct?"),
-                       actionButton("yes", "Yes"),
-                       actionButton("no", "No")
-                       ),
-      textOutput("endtest"),
-      conditionalPanel("input.english",
-                       textOutput("testresult"))
-    
+      fluidRow(
+        column(3),
+        column(4,
+               conditionalPanel("input.submitparameters",
+                                actionButton("spanish", "Spanish")),
+               conditionalPanel("input.spanish",
+                                textOutput("spanishS"))),
+        column(4,
+               conditionalPanel("input.submitparameters",
+                                actionButton("english", "English")),
+               conditionalPanel("input.english",
+                                textOutput("englishS")))
+      ),
+      fluidRow(
+        column(3),
+        column(8, 
+               conditionalPanel("input.english",
+                                helpText("Was your translation correct?"),
+                                actionButton("yes", "Yes"),
+                                actionButton("no", "No")
+               ),
+               textOutput("endtest"),
+               conditionalPanel("input.english",
+                                textOutput("testresult")))
       )
-    
-    
+    )
   )
-))
+
+  
+)
